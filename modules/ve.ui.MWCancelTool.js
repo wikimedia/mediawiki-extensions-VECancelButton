845e45f95b05c3d33e7e9e24dd58e93d2efd35bc
Pavel Astakhov <pastakhov@yandex.ru>
2024-01-15 20:43:52 +0600

    Initial commit
    Change-Id: I18c02a5037f49bec3976e9d78f7d5939f68bad65

---
+++ modules/ve.ui.MWCancelTool.js
@@ -0,0 +1,42 @@
+
+ve.init.mw.targetFactory.on( 'register', function ( name, target ) {
+	if ( name === 'article' ) {
+		target.static.actionGroups.push( {
+			name: 'cancel',
+			type: 'bar',
+			include: ['cancel']
+		} );
+	}
+} );
+
+
+/**
+ * MediaWiki cancel tool
+ *
+ * @class
+ * @abstract
+ * @extends ve.ui.Tool
+ * @constructor
+ * @param {OO.ui.ToolGroup} toolGroup
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.MWCancelTool = function VeUiMWCancelTool() {
+	// Parent constructor
+	ve.ui.MWCancelTool.super.apply( this, arguments );
+};
+
+/* Inheritance */
+OO.inheritClass( ve.ui.MWCancelTool, ve.ui.Tool );
+
+/* Static properties */
+ve.ui.MWCancelTool.static.name = 'cancel';
+ve.ui.MWCancelTool.static.flags = [ 'destructive' ];
+ve.ui.MWCancelTool.static.displayBothIconAndLabel = true;
+ve.ui.MWCancelTool.static.group = 'cancel';
+ve.ui.MWCancelTool.static.commandName = 'cancel';
+ve.ui.MWCancelTool.static.autoAddToCatchall = false;
+ve.ui.MWCancelTool.static.autoAddToGroup = false;
+ve.ui.MWCancelTool.static.title = OO.ui.deferMsg('cancel');
+
+/* Registration */
+ve.ui.toolFactory.register( ve.ui.MWCancelTool );
